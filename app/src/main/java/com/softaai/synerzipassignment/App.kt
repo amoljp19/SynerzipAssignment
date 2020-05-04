302ace26e22216af8a10a3a0355fd0315e072c82
amol pawar <amoljp19@gmail.com>
2020-05-04 14:05:15 +0530

    Resolved koin di issues and adapter list null issue

---
+++ app/src/main/java/com/softaai/synerzipassignment/App.kt
@@ -0,0 +1,35 @@
+package com.softaai.synerzipassignment
+
+import android.content.Context
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
+import com.softaai.synerzipassignment.di.networkModule
+import com.softaai.synerzipassignment.di.persistenceModule
+import com.softaai.synerzipassignment.di.repositoryModule
+import com.softaai.synerzipassignment.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+class App : MultiDexApplication() {
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(listOf(
+                networkModule, persistenceModule,
+                repositoryModule, viewModelModule
+            ))
+        }
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
